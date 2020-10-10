0d54ab473ad2849855a9d9f169fbbd5292222cac
cgalo <cgalo13@gmail.com>
2020-10-09 22:26:09 -0400

    Update Board.h
    Declared the basic structure of the Board class

--- include/Board/Board.h
+++ include/Board/Board.h
@@ -5,9 +5,21 @@
 #ifndef AI_BOARDGAMES_BOARD_H
 #define AI_BOARDGAMES_BOARD_H
 
+#include "../Move/Move.h"
 
 class Board {
 
+private:
+    char ** board;
+    int size;
+
+public:
+    Board(int size, char empty = ' ');      // Constructor, requires a size, defining empty char is optional
+    ~Board();                               // Destructor
+
+    int getSize();                          // Returns the size of the board
+    bool isEmpty();                         // Checks if the board has any empty spaces left
+    Move** getAvailableMoves();             // Returns list of open spots in the board
 };
 
 
